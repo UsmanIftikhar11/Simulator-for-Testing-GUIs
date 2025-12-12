215577ab0a454a07e42f0619d46779b18cc528f2
Wilma Wengborn <wwn22001@student.mdu.se>
2025-12-12 12:21:39 +0100

    Basic Track Movements

---
+++ Assets/TrackMovementLeft.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class TrackMovementLeft : MonoBehaviour
+{
+ 
+    private Renderer rend;
+    public float speed = 2f;
+    private Vector3 lastPos;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        // Use world Y or Z depending on direction you want
+        float offsetY = pos.y * speed;
+        float offsetx = pos.x * speed;
+
+
+        float rotation = transform.eulerAngles.z;
+
+        rend.material.mainTextureOffset = new Vector2(
+            rend.material.mainTextureOffset.x,
+            offsetY - offsetx + rotation
+        );
+    }
+}
+
+
+
+
+
