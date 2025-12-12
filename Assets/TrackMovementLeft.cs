d576693a121acb46516e9a1bb4c363b01304f005
Wilma Wengborn <wwn22001@student.mdu.se>
2025-12-12 13:07:57 +0100

    Better movement

--- Assets/TrackMovementLeft.cs
+++ Assets/TrackMovementLeft.cs
@@ -1,39 +1,43 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class TrackMovementLeft : MonoBehaviour
 {
- 
     private Renderer rend;
-    public float speed = 2f;
+
+    public float moveSpeed = 10f;
+    public float turnSpeed = 10f;
+
     private Vector3 lastPos;
+    private float lastRotZ;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
+        lastPos = transform.position;
+        lastRotZ = transform.eulerAngles.z;
     }
 
     void Update()
     {
-        Vector3 pos = transform.position;
+     
+        Vector3 deltaPos = transform.position - lastPos;
 
-        // Use world Y or Z depending on direction you want
-        float offsetY = pos.y * speed;
-        float offsetx = pos.x * speed;
+  
+        float forwardMovement = Vector3.Dot(deltaPos, transform.up);
 
+        float moveOffset = forwardMovement * moveSpeed;
 
-        float rotation = transform.eulerAngles.z;
+  
+        float deltaRot = Mathf.DeltaAngle(lastRotZ, transform.eulerAngles.z);
 
-        rend.material.mainTextureOffset = new Vector2(
-            rend.material.mainTextureOffset.x,
-            offsetY - offsetx + rotation
-        );
+        float turnOffset = deltaRot * turnSpeed;
+
+        Vector2 off = rend.material.mainTextureOffset;
+        off.y -= moveOffset - turnOffset;
+        rend.material.mainTextureOffset = off;
+
+   
+        lastPos = transform.position;
+        lastRotZ = transform.eulerAngles.z;
     }
 }
-
-
-
-
-
